a293168cc45ca5ce6813d5ebf5071960e9ec8b24
Ezrah-nuby <159791842+Ezrah-nuby@users.noreply.github.com>
2024-02-28 21:44:36 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("what is Your name?")
+age = input("How long have you stayed on earth?")
+location = input("where do you stay on earth?")
+print(f"Good morning {name}, can you confirm that its you are {age} years old and live in {location}")
